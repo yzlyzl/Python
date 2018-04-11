900b034b791a846bab4c60002c813ea2c8571ada
yzlyzl <wuq97@outlook.com>
2018-04-11 21:02:25 +0800

    new commitment

--- data_analysis/linear_regression.py
+++ data_analysis/linear_regression.py
@@ -4,6 +4,7 @@ import os, sys
 import numpy as np
 import pandas as pd
 from pandas import Series, DataFrame
+import matplotlib.pyplot as plt
 
 np.random.seed(12345)
 np.set_printoptions(precision=4)
@@ -39,7 +40,7 @@ def standRegres(xArr, yArr):
 
     '''
     xMat = np.mat(xArr)
-    yMat = np.mat(yArr).T
+    yMat = np.mat(yArr).T    # yArr is a row vector
     xTx = xMat.T * xMat    # the matrix of (X^T * X)
     if np.linalg.det(xTx) == 0.0:
         print ('This matrix is singular, cannot do inverse')
@@ -47,5 +48,37 @@ def standRegres(xArr, yArr):
     ws = xTx.I * (xMat.T * yMat)    # the vector of Regression Coefficient
     return ws
 
+def yPrediction(xArr, ws):
+    '''
+
+        根据计算得到的回归系数ws，来预测y
+
+    '''
+    xMat = np.mat(xArr)
+    yHat = xMat * ws
+    return yHat
+
+def dataPlot(xArr, yArr):
+    '''
+
+        绘制原始数据集的散点图
+
+
+    '''
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    xMat = np.mat(xArr)
+    yMat = np.mat(yArr)
+
+    ax.scatter(xMat[:, 1].flatten().A[0], yMat.T[:, 0].flatten().A[0])
+
+def main():
+    data_file = 'L08-Regression/ex0.txt'
+    xArr, yArr = loadDataSet(data_file)
+    ws = standRegres(xArr, yArr)
+
+    yHat = yPrediction(xArr, ws)
+    dataPlot(xArr, yArr)
+
 if __name__ == '__main__':
-    pass
+    main()
